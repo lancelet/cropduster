2775a1a577e10a468cb723b38ba607ce991edf75
Jonathan Merritt <j.s.merritt@gmail.com>
2023-10-25 22:41:44 +1100

    Move URL to revealjs.com

--- slides/build.sh
+++ slides/build.sh
@@ -7,5 +7,5 @@ pandoc \
   -o index.html \
   slides.md \
   --include-in-header=slides.css \
-  -V revealjs-url=https://unpkg.com/reveal.js/ \
+  -V revealjs-url=https://revealjs.com/ \
   -V theme=solarized
